84b3ce916414aa49ace8893819578912f3709d7d
shreyash-mehta <shreyashmehta001@gmail.com>
2025-02-14 02:17:55 +0530

    exploring get, post, delete

--- src/app.js
+++ src/app.js
@@ -3,12 +3,30 @@ const app = express();
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
 });
-// app.use("/", (req, res) => {
-//     res.send("Hello from the server!");
-//   });
+
+
+app.get("/test", (req, res) => {
+  res.send({ firstName: "Shreyash", lastName: "Mehta" });
+});
+
+app.post("/test", (req, res) => {
+  res.send("Data saved successfully");
+});
+
+app.delete("/test", (req, res) => {
+  res.send("Data deleted successfully");
+});
+
 app.use("/test", (req, res) => {
   res.send("Hello 10000 times from the test server!");
 });
+
+
+
+// app.use("/", (req, res) => {
+//     res.send("Hello from the server!");
+//   });
+
 app.use("/demo", (req, res) => {
-    res.send("Hello from the demo server!");
-  });
+  res.send("Hello from the demo server!");
+});
