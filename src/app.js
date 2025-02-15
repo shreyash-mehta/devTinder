97294f41c7513d526de67960f097a485fa2e373f
EdenHazardd <ritikmehta123@gmail.com>
2025-02-15 22:29:32 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -4,20 +4,8 @@ app.listen(3000, () => {
   console.log("Server is listening on port 3000");
 });
 
-
-app.get("/test", (req, res) => {
-  res.send({ firstName: "Shreyash", lastName: "Mehta" });
-});
-
-app.post("/test", (req, res) => {
-  res.send("Data saved successfully");
-});
-
-app.delete("/test", (req, res) => {
-  res.send("Data deleted successfully");
-});
-
-app.use("/test", (req, res) => {
+app.use("/user/:userId/:name/:city", (req, res) => {
+  console.log(req.params);
   res.send("Hello 10000 times from the test server!");
 });
 
@@ -27,6 +15,6 @@ app.use("/test", (req, res) => {
 //     res.send("Hello from the server!");
 //   });
 
-app.use("/demo", (req, res) => {
-  res.send("Hello from the demo server!");
-});
+// app.use("/demo", (req, res) => {
+//   res.send("Hello from the demo server!");
+// });
