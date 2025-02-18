daa97fcb147f4eaf53839b48131c92dc8b0a3b92
shreyash-mehta <shreyashmehta001@gmail.com>
2025-02-19 02:49:03 +0530

    get APIs

--- src/app.js
+++ src/app.js
@@ -7,14 +7,45 @@ const app = express();
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-  
   const user = new User(req.body);
-  try{
+  try {
     await user.save();
     res.send("User is added successfully!");
+  } catch (err) {
+    res.status(500).send("Error saving the user:", +err.message);
   }
-  catch(err){
-    res.status(500).send("Error saving the user:", + err.message);
+});
+
+app.get("/user", async (req, res) => {
+  const userEmailId = req.body.emailId;
+
+  try {
+    const user = await User.findOne();
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+  } 
+  // try {
+  //   const users = await User.find({ emailId: userEmailId });
+  //   if (users.length === 0) {
+  //     res.status(404).send("User not found");
+  //   } else {
+  //     res.send(users);
+  //   }
+  // } 
+  catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(404).send("Something went wrong");
   }
 });
 
