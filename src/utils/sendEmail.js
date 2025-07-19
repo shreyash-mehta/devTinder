52ded7f3dcf54edc50881c19bc31dc05bd3289c3
shreyash-mehta <shreyashmehta001@gmail.com>
2025-07-20 02:44:11 +0530

    corrected errors

---
+++ src/utils/sendEmail.js
@@ -0,0 +1,51 @@
+const { SendEmailCommand } = require("@aws-sdk/client-ses");
+const { sesClient } = require("./sesClient.js");
+
+const createSendEmailCommand = (toAddress, fromAddress) => {
+  return new SendEmailCommand({
+    Destination: {
+      CcAddresses: [],
+      ToAddresses: [toAddress],
+    },
+    Message: {
+      Body: {
+        Html: {
+          Charset: "UTF-8",
+          Data: "<h1>This is the email body</h1>",
+        },
+        Text: {
+          Charset: "UTF-8",
+          Data: "This is the text format email",
+        },
+      },
+      Subject: {
+        Charset: "UTF-8",
+        Data: "Welcom to devTinder",
+      },
+    },
+    Source: fromAddress,
+    ReplyToAddresses: [
+      /* more items */
+    ],
+  });
+};
+
+const run = async () => {
+  const sendEmailCommand = createSendEmailCommand(
+    "shreyashmehta001@gmail.com",
+    "support@devtinder.org"
+  );
+
+  try {
+    return await sesClient.send(sendEmailCommand);
+  } catch (caught) {
+    if (caught instanceof Error && caught.name === "MessageRejected") {
+      const messageRejectedError = caught;
+      return messageRejectedError;
+    }
+    throw caught;
+  }
+};
+
+// snippet-end:[ses.JavaScript.email.sendEmailV3]
+module.exports = { run };
