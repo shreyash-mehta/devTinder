c22a74205a23a7d45326532619aa83c95953d3c4
shreyash-mehta <shreyashmehta001@gmail.com>
2025-07-22 03:02:04 +0530

    scheduling cron jobs

--- src/utils/cron.js
+++ src/utils/cron.js
@@ -1,7 +1,41 @@
-
 const cron = require("node-cron");
+const ConnectionRequestModel = require("../models/connectionRequest");
+const { subDays, startOfDay, endOfDay } = require("date-fns");
+const sendEmail = require("./sendEmail");
 
-cron.schedule("* 8 * * *", () => {
-    const yesterday = new Date() - 1;
-    
+cron.schedule("0 8 * * *", async () => {
+  try {
+    const yesterday = subDays(new Date(), 1);
+    const yesterdayStart = startOfDay(yesterday);
+    const yesterdayEnd = endOfDay(yesterday);
+
+    const pendingRequests = await ConnectionRequestModel.find({
+      status: "interested",
+      createdAt: {
+        $gte: yesterdayStart,
+        $lt: yesterdayEnd,
+      },
+    }).populate("fromUserId toUserId");
+
+    const listOfEmails = [
+      ...new Set(pendingRequests.map((req) => req.toUserId.emailId)),
+    ];
+
+    console.log(listOfEmails);
+
+    for (const email of listOfEmails) {
+      try {
+        const res = await sendEmail.run(
+          "Pending connection requests on devTinder",
+          email +
+            " you have new connection requests pending to be reviewed on devTinder"
+        );
+        console.log(res);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  } catch (err) {
+    console.log(err);
+  }
 });
