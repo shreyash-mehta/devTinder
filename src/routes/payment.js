41f39769c7909f44f634793e9254e35063c9aa59
shreyash-mehta <shreyashmehta001@gmail.com>
2025-07-24 03:19:03 +0530

    bug fixes

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -83,13 +83,12 @@ paymentRouter.post("/payment/webhook", async (req, res) => {
   }
 });
 
-paymentRouter.get("/payment/verify", userAuth, async (req, res) => {
+paymentRouter.get("/premium/verify", userAuth, async (req, res) => {
   const user = req.user.toJSON();
   if (user.isPremium) {
     return res.json({ ...user });
-  } else {
-    return res.json({  ...user });
   }
+  return res.json({ ...user });
 });
 
 module.exports = paymentRouter;
